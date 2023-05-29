2246b5655f3760f65c715c7bbc00501cbfab0530
lintd <lintd23@outlook.com>
2023-05-29 22:37:09 +0800

    :accept: AC start91d G

---
+++ start91d/g/main.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+// Placeholder for upcoming un-std algorithm, by rspc
+// End placeholder for upcoming un-std algorithm, by rspc
+// Scheme by rspc:
+
+#if defined(RSPC_TRACE_HINT)
+#define TRACE(...) do { __VA_ARGS__; } while (0)
+#else
+#define TRACE(...) (void)0
+#endif
+#define TRACELN(...) TRACE(__VA_ARGS__; cout << endl)
+#define TRACE_VEC(VEC, ITEM, ...) TRACE(cout << #VEC << "= "; for_each((VEC).begin(), (VEC).end(), [&](ITEM _i) { __VA_ARGS__; }))
+using namespace std;
+#define self_todo_placeholder
+
+
+int main() {
+#if defined(RSPC_TRACE_BTIME)
+    TRACE(cout << "build time: " RSPC_TRACE_BTIME "\n");
+#else
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+#endif
+
+    int tt; cin >> tt; while (tt--) {
+        uint64_t l, r, ans = 0; cin >> l >> r;
+        if (0 == l) ++ans;
+        for (uint64_t t = 1; r > 0; t <<= 1, r >>= 1) {
+            if (t > l) ans += t - l;
+        }
+        cout<<ans <<endl;
+    }
+    return 0;
+}
+
