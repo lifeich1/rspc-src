71a8e5b2540f9f3edaa440a4a822901aa3434a04
lintd <lintd23@outlook.com>
2023-06-04 21:57:42 +0800

    :accept: AC abc304/f (#17)

--- abc304/f/main.cc
+++ abc304/f/main.cc
@@ -30,6 +30,7 @@ int main() {
     int n12 = n / 2;
     int64_t ans = 0;
     
+    map<int,int64_t> aa;
     for (int i = 1; i <= n12; ++i) {
         if (n % i != 0) continue;
         fill(u,u+i, 0);
@@ -42,8 +43,15 @@ int main() {
                 t <<=1;
                 if (t >= M) t -= M;
             }
-        ans += t;
-        if (ans >= M) ans -= M;
+        aa[i] = t;
+    }
+    for (auto [k,v] : aa) {
+        ans = (ans +v)%M;
+        for (auto & [kk, vv] : aa) {
+            if (kk > k && kk % k == 0) {
+                vv = (vv + M - v) % M;
+            }
+        }
     }
     cout<< ans<<endl;
     return 0;
