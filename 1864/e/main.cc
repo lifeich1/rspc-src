e3761a89fef2e24a93c61c4ec334304fe0a8df30
lintd <lintd23@outlook.com>
2023-08-27 17:30:56 +0800

    :accept: AC 1864 E (#56)

--- 1864/e/main.cc
+++ 1864/e/main.cc
@@ -1,56 +1,81 @@
 #include <bits/stdc++.h>
 
 // Placeholder for upcoming un-std algorithm, by rspc
-namespace A {
-template <class T> inline constexpr T qpow_in_mod(T base, T ind, T m) {
-  T res = 1;
-  for (T bi = base; ind > 0; ind >>= 1, bi = (bi * bi) % m)
-    if (ind & 1)
-      res = (res * bi) % m;
-  return res;
+template <class T> int64_t qpow64(int64_t v, T k, int64_t m) {
+  int64_t r = 1;
+  for (; k > 0; k >>= 1, v = (v * v) % m)
+    if (k & 1)
+      r = (r * v) % m;
+  return r;
 }
-template <int64_t M, class T = int64_t> class CalcInMod {
-public:
-  typedef T data_t;
-  T v;
-  CalcInMod() {}
-  CalcInMod(int v) : v{static_cast<T>(v)} {}
-  CalcInMod(T v) : v{v} {}
-  int as_i() { return static_cast<int>(v); }
-  using type = CalcInMod<M, T>;
-  inline static T add(T a, T b) { return (a + b) % M; }
-  inline static T sub(T a, T b) { return (a - b + M) % M; }
-  inline static T mul(T a, T b) { return (a * b) % M; }
-  inline static T qpow(T base, T ind) {
-    T res = 1;
-    for (T bi = base; ind > 0; ind >>= 1, bi = mul(bi, bi))
-      if (ind & 1)
-        res = mul(res, bi);
-    return res;
+template <typename Base> struct number_interface : public Base {
+  typedef number_interface<Base> Self;
+  template <typename... Args>
+  number_interface(Args... args) : Base(std::forward<Args>(args)...) {}
+  Self get_inv() const {
+    Self r = *this;
+    r.inv();
+    return r;
   }
-  inline type operator+(type const &a) const { return {(a.v + v) % M}; }
-  inline type operator-(type const &a) const { return {(v - a.v + M) % M}; }
-  inline type operator*(type const &a) const { return {(a.v * v) % M}; }
-  inline type &operator+=(type const &a) {
-    *this = *this + a;
+  void div(Self const &rhs) { this->mul(rhs.get_inv()); }
+  Self &operator+=(Self const &other) {
+    this->add(other);
     return *this;
   }
-  inline type &operator-=(type const &a) {
-    *this = *this - a;
+  Self &operator*=(Self const &other) {
+    this->mul(other);
     return *this;
   }
-  inline type &operator*=(type const &a) {
-    *this = *this * a;
+  Self &operator/=(Self const &other) {
+    this->div(other);
     return *this;
   }
-  inline type mul_inv() const { return {qpow(M - 2)}; }
-  inline type qpow(T ind) const { return {qpow(v, ind)}; }
-  inline bool operator==(type const &a) const { return v == a.v; }
-  inline bool operator<(type const &a) const { return v < a.v; }
+  Self &operator-=(Self const &other) {
+    this->sub(other);
+    return *this;
+  }
+  Self operator+(Self const &other) const {
+    Self r{*this};
+    r += other;
+    return r;
+  }
+  Self operator-(Self const &other) const {
+    Self r{*this};
+    r -= other;
+    return r;
+  }
+  Self operator*(Self const &other) const {
+    Self r{*this};
+    r *= other;
+    return r;
+  }
+  Self operator/(Self const &other) const {
+    Self r{*this};
+    r /= other;
+    return r;
+  }
+  bool operator<(Self const &rhs) const { return this->cmp(rhs) < 0; }
+  bool operator>(Self const &rhs) const { return this->cmp(rhs) > 0; }
+  bool operator==(Self const &rhs) const { return this->cmp(rhs) == 0; }
+  bool operator<=(Self const &rhs) const { return this->cmp(rhs) == 0; }
+  bool operator>=(Self const &rhs) const { return this->cmp(rhs) >= 0; }
 };
-} // namespace A
+template <int64_t M> struct modint_impl {
+  typedef modint_impl<M> Self;
+  int64_t v;
+  modint_impl() : v{0} {}
+  template <class T> explicit modint_impl(T t) : v{t} {}
+  void add(Self const &rhs) { v = (v + rhs.v) % M; }
+  void sub(Self const &rhs) { v = (v + M - rhs.v) % M; }
+  void mul(Self const &rhs) { v = (v * rhs.v) % M; }
+  void inv() { v = qpow64(v, M - 2, M); }
+  int cmp(Self const &rhs) const {
+    return v == rhs.v ? 0 : (v < rhs.v ? -1 : 1);
+  }
+};
+template <int64_t M> using modint = number_interface<modint_impl<M>>;
 // End placeholder for upcoming un-std algorithm, by rspc
-// Scheme by rspc: calc_in_mod
+// Scheme by rspc: qpow64 number_interface modint
 
 #if defined(RSPC_TRACE_HINT)
 #define TRACE(...)                                                             \
@@ -71,7 +96,9 @@ public:
 using namespace std;
 #define self_todo_placeholder
 
-typedef A::CalcInMod<998244353> Cm;
+//     typedef A::CalcInMod<998244353> Cm;
+typedef modint<998244353> mint;
+map<int, int> m[31];
 
 const int N = 201001;
 int a[N];
@@ -88,36 +115,40 @@ int main() {
   while (tt--) {
     int n;
     cin >> n;
-    map<int, int> m;
+    for (int i = 0; i < 30; ++i)
+      m[i].clear();
     for (int i = 0; i < n; ++i) {
       int x;
       cin >> x;
       a[i] = x;
-      m[x]++;
-      while (x > 0) {
-        x -= x & (-x);
-        if (x > 0)
-          m[x]++;
+      for (int i = 0; i < 30; ++i) {
+        m[i][x]++;
+        x >>= 1;
       }
     }
-    Cm ans = n;
-    ans *= n;
-    for (int i = 0; i < n; ++i) {
-      int x = a[i];
-      while (x > 0) {
-        ans += m[x];
-        x -= x & (-x);
+    mint ans = 0;
+    auto g = [&](int i, int x) {
+      if (m[i].count(x) == 0)
+        return 0;
+      return m[i][x];
+    };
+    for (int i = 0; i < 30; ++i) {
+      for (auto [k, v] : m[i]) {
+        auto v1 = g(i, k ^ 1);
+        int t = __builtin_popcount(k - (k & 1)) + 1;
+        if (k & 1) { // a > b
+          t += t & 1;
+        } else { // a < b
+          t += (t & 1) ^ 1;
+        }
+        ans += mint(t) * v * v1;
       }
-      TV(ans.v);
-      for (int k = 2; k <= a[i]; k <<= 1) {
-        ans += m[k / 2];
-      }
-      TLN(TV(i); TV(ans.v); TV(a[i]));
     }
-    Cm rn = Cm{n}.mul_inv();
-    TLN(TV(ans.v));
-    ans *= rn;
-    ans *= rn;
+    for (auto [k, v] : m[0]) {
+      mint t = __builtin_popcount(k) + 1;
+      ans += mint(t) * v * v;
+    }
+    ans /= mint(n) * n;
     cout << ans.v << endl;
   }
   return 0;
