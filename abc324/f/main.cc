ef842e39d30cecff52e4d0e88ddc1994db619d99
lintd <lintd23@outlook.com>
2023-10-15 23:18:06 +0800

    :accept: abc324 f (close #77)

--- abc324/f/main.cc
+++ abc324/f/main.cc
@@ -24,38 +24,30 @@ using namespace std;
 #define self_todo_placeholder
 
 const int N = 201001;
-int pa[N], sz[N];
-vector<int> es[N];
-int &fa(int r) {
-  if (pa[r] == r)
-    return pa[r];
-  int &p = fa(pa[r]);
-  pa[r] = p;
-  return p;
-}
-
-void uni(int x, int y) {
-  x = fa(x), y = fa(y);
-  if (sz[x] < sz[y])
-    swap(x, y);
-  sz[x] += sz[y];
-  fa(y) = x;
-}
 
 vector<int> u, v, b, c;
-vector<double> bc;
-int sb = 0, sc = 0;
-bool dfs(int r, int ta) {
-  if (r == ta)
-    return true;
-  for (auto e : es[r]) {
-    if (dfs(v[e], ta)) {
-      TLN(TV(e));
-      sb += b[e], sc += c[e];
-      return true;
+vector<int> es[N];
+int g[N];
+double f[N];
+int n, m;
+
+bool check(double K) {
+  f[1] = 0;
+  g[1] = 1;
+  fill(g + 2, g + n + 1, 0);
+  for (int i = 1; i < n; ++i) {
+    if (0 == g[i])
+      continue;
+    for (auto e : es[i]) {
+      double t = f[i] + b[e] - K * c[e];
+      int y = v[e];
+      if (0 == g[y] || f[y] < t) {
+        f[y] = t;
+        g[y] = 1;
+      }
     }
   }
-  return false;
+  return g[n] && f[n] >= 0;
 }
 
 int main() {
@@ -66,13 +58,11 @@ int main() {
   cin.tie(0), cout.tie(0);
 #endif
 
-  int n, m;
   cin >> n >> m;
   u.reserve(m);
   v.reserve(m);
   b.reserve(m);
   c.reserve(m);
-  bc.reserve(m);
   for (int i = 0; i < m; ++i) {
     int x, y, z, w;
     cin >> x >> y >> z >> w;
@@ -80,28 +70,17 @@ int main() {
     v.emplace_back(y);
     b.emplace_back(z);
     c.emplace_back(w);
+    es[x].emplace_back(i);
   }
-  vector<int> h(m, 0);
-  for (int i = 0; i < m; ++i)
-    h[i] = i;
-  sort(h.begin(), h.end(), [&](auto l, auto r) {
-    int p = b[l] * c[r], q = b[r] * c[l];
-    return p > q || (p == q && b[l] > b[r]);
-  });
-  for (int i = 1; i <= n; ++i)
-    pa[i] = i, sz[i] = 1;
-
-  for (auto e : h) {
-    int x = fa(u[e]), y = fa(v[e]);
-    if (x == y)
-      continue;
-    es[u[e]].emplace_back(e);
-    uni(x, y);
+  double l = 0, r = 1e4;
+  for (int _it = 0; _it < 100; ++_it) {
+    auto t = (l + r) / 2;
+    if (check(t))
+      l = t;
+    else
+      r = t;
   }
-  dfs(1, n);
-  double d = sb;
-  d /= sc;
-  cout << std::fixed << setprecision(20) << d << endl;
+  cout << fixed << setprecision(16) << l << endl;
   return 0;
 }
 
