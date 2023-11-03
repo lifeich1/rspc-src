0ed23f4bf692a2e08bf2dff5ac823445ca936ef1
lintd <lintd23@outlook.com>
2023-11-04 04:18:31 +0800

    :hammer: find issue num for tada

--- .tools/prepare-msg.pl
+++ .tools/prepare-msg.pl
@@ -8,60 +8,72 @@ my @lines = split /\n/, `git status -s`;
 say scalar @lines;
 
 my %emo = (
-    tada => q/:tada:/,
-    ac   => q/:accept:/,
+  tada => q/:tada:/,
+  ac   => q/:accept:/,
 );
 my $tg  = shift;
 my $emo = $emo{$tg};
 
 sub gac {
-    my ( $n, $msg ) = @_;
-    system( ( qw{git add}, $n ) ) == 0 or die "git add $n failed: $?";
-    system( ( qw{git commit -m}, qq{$emo $msg}, '-e' ) ) == 0
-      or die "git commit $n failed: $?";
-    say "Done commit: $n";
-    exit 0;
+  my ( $n, $msg ) = @_;
+  system( ( qw{git add}, $n ) ) == 0 or die "git add $n failed: $?";
+  system( ( qw{git commit -m}, qq{$emo $msg}, '-e' ) ) == 0
+    or die "git commit $n failed: $?";
+  say "Done commit: $n";
+  exit 0;
 }
 
 sub gissue {
-    my ($n) = @_;
-    chomp( my $line = qx(git log --oneline -20| grep "$n" | head -n 1) );
-    say "gissue n:$n line: $line";
-    if ( $line =~ m{:accept: (?:AC )?$n \w+ \(#(\d+)\)} ) {
-        return $1;
-    }
-    elsif ( $line =~ m{:tada: (?:\[\w+\] )$n \(#(\d+)\)} ) {
-        return $1;
-    }
+  my ($n) = @_;
+  chomp( my $line = qx(git log --oneline -20| grep "$n" | head -n 1) );
+  say "gissue n:$n line: $line";
+  if ( $line =~ m{:accept: (?:AC )?$n \w+ \(#(\d+)\)} ) {
+    return $1;
+  }
+  elsif ( $line =~ m{:tada: (?:\[\w+\] )$n \(#(\d+)\)} ) {
+    return $1;
+  }
+}
+
+sub gh_isu_n {
+  my ($name) = @_;
+  chomp( my $line = qx(gh issue list | grep "CP: $name") );
+  if ( $line =~ /\A(\d+)\s/ ) {
+    return $1;
+  }
+  return '';
 }
 
 foreach (@lines) {
-    if (/^[\s\w]{2}\s(\S+)/) {
-        my $n = $1;
-        say "handling $n ...";
+  if (/^[\s\w]{2}\s(\S+)/) {
+    my $n = $1;
+    say "handling $n ...";
 
-        if ( $tg eq 'ac' ) {
-            if ( $n =~ m{\A(\w+)/(\w)/main\.(?:cc|rs)\Z} ) {
-                my $i = &gissue($1);
-                &gac( $n, qq{$1 $2 (#$i)} );
-            }
-        }
-        elsif ( $tg eq 'tada' ) {
-            if ( $n =~ m{\A(leetcode/(?:bi)?weekly-contest-\d+\.txt)\Z} ) {
-                &gac( $n, $1 );
-            }
-            if ( $n =~ m{\A(\d+)/(\w)/main\.(?:cc|rs)\Z} ) {
-                &gac( $n, "$1 (#)" );
-            }
-            if ( $n =~ m{\A(\D\w+)/(\w)/main\.(?:cc|rs)\Z} ) {
-                &gac( $n, "[atcoder] $1 (#)" );
-            }
-            if ( $n =~ m{\A(START\w+)/(\w+)/main\.(?:cc|rs)\Z} ) {
-                &gac( $n, "[codechef] $1 (#)" );
-            }
-        }
-        else {
-            die "unknown arg1: $tg";
-        }
+    if ( $tg eq 'ac' ) {
+      if ( $n =~ m{\A(\w+)/(\w)/main\.(?:cc|rs)\Z} ) {
+        my $i = &gissue($1);
+        &gac( $n, qq{$1 $2 (#$i)} );
+      }
     }
+    elsif ( $tg eq 'tada' ) {
+      if ( $n =~ m{\A(leetcode/(?:bi)?weekly-contest-\d+\.txt)\Z} ) {
+        &gac( $n, $1 );
+      }
+      if ( $n =~ m{\A(\d+)/(\w)/main\.(?:cc|rs)\Z} ) {
+        my $sn = &gh_isu_n($1);
+        &gac( $n, "$1 (#$sn)" );
+      }
+      if ( $n =~ m{\A(\D\w+)/(\w)/main\.(?:cc|rs)\Z} ) {
+        my $sn = &gh_isu_n($1);
+        &gac( $n, "[atcoder] $1 (#sn)" );
+      }
+      if ( $n =~ m{\A(START\w+)/(\w+)/main\.(?:cc|rs)\Z} ) {
+        my $sn = &gh_isu_n($1);
+        &gac( $n, "[codechef] $1 (#$sn)" );
+      }
+    }
+    else {
+      die "unknown arg1: $tg";
+    }
+  }
 }
