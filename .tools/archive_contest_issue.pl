84b00b783899b1f5e53595bfbfea96ae06997cb6
lintd <lintd23@outlook.com>
2023-12-10 01:47:48 +0800

    :hammer: better issue num search

--- .tools/archive_contest_issue.pl
+++ .tools/archive_contest_issue.pl
@@ -13,9 +13,19 @@ foreach (@line) {
   say STDERR "line: $_";
   if (m{:accept: (?:AC )?$tg \w+ \(#(\d+)\)}) {
     say $1;
-    last;
+    exit;
   }
   elsif (m{:tada: (?:\[\w+\] )?$tg \(#(\d+)\)}) {
+    say $1;
+    exit;
+  }
+}
+say STDERR "fallback to slow search ...";
+
+my @line = split /\n/, qx(gh issue list| grep "$tg");
+foreach (@line) {
+  say STDERR "line: $_";
+  if (/(\d+)\s+OPEN\s+CP: $tg\s+/) {
     say $1;
     last;
   }
